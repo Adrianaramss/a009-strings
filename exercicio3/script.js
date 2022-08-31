e391c3e7ae0e9a21dfe2821e97aa3658e9ae52d4
Adriana <adrianascosta9@gmail.com>
2022-08-31 16:17:59 -0300

    exerciciocompleto

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,15 @@
-//Crie a const para a frase aqui
+
+//exercicio A
+const frase = "Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\""
+console.log (frase) 
+
+//exercicio B
+const frasemotificada = frase.replace ("verde", "rosa") . replace ("azul", "laranja")
+console.log (frasemotificada)
+
+//exercicio C
+let possuiverde = frasemotificada.includes("verde")
+let possuiazul = frasemotificada.includes ("azul")
+console.log ( `possui a cor verde? ${possuiverde} \n possui a cor azul? ${possuiazul} `)
+
+///
